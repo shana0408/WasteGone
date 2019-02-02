4de48577355e7749acfd1f4bd05a5d0fd09b02f9
shana0408 <32724361+shana0408@users.noreply.github.com>
2019-02-02 16:36:27 +0800

    change first load page to login page

--- app/src/main/java/com/ntu/cz2006/wastegone/ui/MainActivity.java
+++ app/src/main/java/com/ntu/cz2006/wastegone/ui/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void openMap() {
-        Intent intent = new Intent(this, MapsActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 }
