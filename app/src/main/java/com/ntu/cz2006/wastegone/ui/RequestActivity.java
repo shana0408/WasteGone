2f3f9625472880d7945b6f4286fb37e74f1affeb
shana0408 <32724361+shana0408@users.noreply.github.com>
2019-02-24 17:29:59 +0800

    add request activity

---
+++ app/src/main/java/com/ntu/cz2006/wastegone/ui/RequestActivity.java
@@ -0,0 +1,37 @@
+package com.ntu.cz2006.wastegone.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.ntu.cz2006.wastegone.R;
+
+public class RequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home)
+        {
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
